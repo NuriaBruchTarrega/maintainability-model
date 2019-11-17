09069d99166866ac611d372fcb3334acd4f7589a
Jakob Löhnertz <mail@jakob.codes>
2019-11-17 23:20:21 +0100

    Add the scaffold for the Duplication metric

--- src/lib/java/Utility.java
+++ src/lib/java/Utility.java
@@ -1,21 +1,41 @@
 package lib.java;
 
-import java.math.BigInteger;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-import io.usethesource.vallang.*;
+import org.rascalmpl.interpreter.utils.RuntimeExceptionFactory;
 
+import io.usethesource.vallang.*;
 
 public class Utility {
 	protected final IValueFactory values;
-	
-	public Utility(IValueFactory values){
+
+	public Utility(IValueFactory values) {
 		this.values = values;
 	}
-	
-	public IValue md5HashFile(IValue input) throws NoSuchAlgorithmException {
-		MessageDigest md = MessageDigest.getInstance("MD5");
-		return values.string(md.digest("Hello"));
+
+	/**
+	 * Creates MD5 hash of a string input.
+	 * 
+	 * @author Cleverton Hentz
+	 * @see https://github.com/usethesource/rascal/pull/1056
+	 */
+	public IString hashMD5(IString in) throws IOException {
+		byte[] hash;
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			md.update(StandardCharsets.UTF_8.encode(in.getValue()));
+			hash = md.digest();
+		} catch (NoSuchAlgorithmException e) {
+			throw RuntimeExceptionFactory.io(values.string("Cannot load MD5 digest algorithm"), null, null);
+		}
+
+		StringBuffer result = new StringBuffer(hash.length * 2);
+		for (int i = 0; i < hash.length; i++) {
+			result.append(Integer.toString((hash[i] & 0xff) + 0x100, 16).substring(1));
+		}
+		return values.string(result.toString());
 	}
 }
